9f158968630a1431b983e59bd7a11d8ecaa964c1
yiminghe <yiminghe@gmail.com>
2010-09-16 22:00:01 +0800

    give ul marker by level

--- build/editor-all-pkg.js
+++ build/editor-all-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-09-16 21:41:31
+ * @buildtime: 2010-09-16 21:59:25
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
