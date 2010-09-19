dc6cd5abff6e1053a84f70620e10d29da2d163db
yiminghe <yiminghe@gmail.com>
2010-09-19 16:06:25 +0800

    fix google theme

--- build/editor-all-pkg.js
+++ build/editor-all-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-09-19 15:29:13
+ * @buildtime: 2010-09-19 16:05:59
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
