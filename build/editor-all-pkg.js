bb65ec09608722ce9b5522a1a6fac4e0b24b25c5
yiminghe <yiminghe@gmail.com>
2010-09-27 21:42:05 +0800

    fix cool theme

--- build/editor-all-pkg.js
+++ build/editor-all-pkg.js
@@ -2,11 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
-<<<<<<< HEAD
- * @buildtime: 2010-09-27 21:26:56
-=======
- * @buildtime: 2010-09-27 21:29:47
->>>>>>> master
+ * @buildtime: 2010-09-27 21:41:48
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
