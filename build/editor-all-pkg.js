e238f9f8dc9830e0736a9edeb43685fa8013086b
yiminghe <yiminghe@gmail.com>
2010-09-30 17:13:28 +0800

    demo updated

--- build/editor-all-pkg.js
+++ build/editor-all-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-09-30 17:05:08
+ * @buildtime: 2010-09-30 17:13:11
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
