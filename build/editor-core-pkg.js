5378f720219652d683b2ca430104644526bd9c40
yiminghe <yiminghe@gmail.com>
2010-10-08 16:25:49 +0800

    tabs added

--- build/editor-core-pkg.js
+++ build/editor-core-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-10-08 16:22:38
+ * @buildtime: 2010-10-08 16:25:23
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
