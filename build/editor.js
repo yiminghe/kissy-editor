43513c549747b757cd1e6f18b27707e01f133246
yiminghe <yiminghe@gmail.com>
2010-10-09 17:10:17 +0800

    bangpai code added

--- build/editor.js
+++ build/editor.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-10-09 17:05:10
+ * @buildtime: 2010-10-09 17:09:40
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
