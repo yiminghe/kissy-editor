b65cae3e0c1e5e856f1ceb1dafd2885054ca533a
yiminghe <yiminghe@gmail.com>
2010-10-15 17:21:39 +0800

    use cdn

--- build/editor-core-pkg.js
+++ build/editor-core-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-10-15 17:12:55
+ * @buildtime: 2010-10-15 17:19:39
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
