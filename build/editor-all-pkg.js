95b52f08ba1f032306302deb85edcb429f5d4d53
yiminghe <yiminghe@gmail.com>
2010-10-19 14:48:33 +0800

    fix overlay hd in ie6

--- build/editor-all-pkg.js
+++ build/editor-all-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-10-19 14:40:22
+ * @buildtime: 2010-10-19 14:48:06
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
