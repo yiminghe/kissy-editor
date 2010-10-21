4ffd576553450c9094369d6f0fdef192f5c13a85
yiminghe <yiminghe@gmail.com>
2010-10-21 21:40:36 +0800

    fix table border parse

--- src/plugins/table/plugin.js
+++ src/plugins/table/plugin.js
@@ -437,10 +437,13 @@ KISSY.Editor.add("table", function(editor, undefined) {
                         caption = selectedTable.one("caption");
 
 
-                    d.talign.val(selectedTable.attr("align") || "");
+                    d.talign.val(selectedTable.attr("align") ||
+                        "");
 
-                    d.tborder.val(selectedTable.attr("border") || "");
-                    var w = selectedTable._4e_style("width") || "";
+                    d.tborder.val(selectedTable.attr("border") ||
+                        "0");
+                    var w = selectedTable._4e_style("width") ||
+                        ("" + selectedTable.width());
                     //忽略pt单位
                     d.twidth.val(w.replace(/px|%|(.*pt)/i, ""));
                     if (w.indexOf("%") != -1) d.twidthunit.val("%");
