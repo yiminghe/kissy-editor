1e7c4165e188fedfbaaed46a1ad6a436f4167aca
yiminghe <yiminghe@gmail.com>
2010-10-28 15:43:19 +0800

    yunlie demo added

--- build/editor-core-pkg.js
+++ build/editor-core-pkg.js
@@ -2,7 +2,7 @@
  * Constructor for kissy editor and module dependency definition
  * @author: yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2.0
- * @buildtime: 2010-10-28 15:33:52
+ * @buildtime: 2010-10-28 15:42:53
  */
 KISSY.add("editor", function(S, undefined) {
     var DOM = S.DOM;
@@ -295,10 +295,10 @@ KISSY.add("editor", function(S, undefined) {
             charset:"utf-8",
             requires: mod.requires,
             csspath: (mod.useCss ? debugUrl("plugins/" + name + "/plugin.css?t=" +
-                encodeURIComponent("2010-10-28 15:33:52")+
+                encodeURIComponent("2010-10-28 15:42:53")+
                 "") : undefined),
             path: debugUrl("plugins/" + name + "/plugin.js?t=" +
-                encodeURIComponent("2010-10-28 15:33:52")+
+                encodeURIComponent("2010-10-28 15:42:53")+
                 "")
         };
     }
