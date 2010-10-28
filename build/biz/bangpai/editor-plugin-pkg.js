fc4d6730faec19a1b8bb1b4712c1b9fe7990ec9c
yiminghe <yiminghe@gmail.com>
2010-10-28 11:39:31 +0800

    menu demo updated

--- build/biz/bangpai/editor-plugin-pkg.js
+++ build/biz/bangpai/editor-plugin-pkg.js
@@ -134,7 +134,7 @@ KISSY.Editor.add("bangpai-music", function(editor) {
                     path:KE.Utils.debugUrl(
                         "biz/bangpai/plugins/music/" +
                             "dialog/plugin.js?t=" +
-                           encodeURIComponent("2010-10-28 11:35:51")+
+                           encodeURIComponent("2010-10-28 11:39:12")+
                             ""
                         )
                 }
@@ -273,7 +273,7 @@ KISSY.Editor.add("bangpai-upload", function(editor) {
                     path:KE.Utils.debugUrl(
                         "biz/bangpai/plugins/upload/" +
                             "dialog/plugin.js?t=" +
-                            encodeURIComponent("2010-10-28 11:35:51")+
+                            encodeURIComponent("2010-10-28 11:39:12")+
                             ""
                         )
                 }
@@ -442,7 +442,7 @@ KISSY.Editor.add("bangpai-video", function(editor) {
                     path:KE.Utils.debugUrl(
                         "biz/bangpai/plugins/video/" +
                             "dialog/plugin.js?t=" +
-                            encodeURIComponent("2010-10-28 11:35:51") +
+                            encodeURIComponent("2010-10-28 11:39:12") +
                             ""
                         )
                 }
