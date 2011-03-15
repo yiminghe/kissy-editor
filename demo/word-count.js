77ab875602749a65be964fb4f4088b5065500adf
yiminghe <yiminghe@gmail.com>
2011-03-15 18:57:47 +0800

    update wordcount

--- demo/word-count.js
+++ demo/word-count.js
@@ -1,15 +1,23 @@
-//字数模块 by 龙啸
+/*
+ 字数统计插件
+ @author:龙啸
+ */
 (function() {
     var S = KISSY;
 
     S.namespace('wordcount');
 
-    //参数：textarea的ID，初始化编辑器源代码数，最大限制数，编辑器editor对象
-    KISSY.wordcount.bind = function(textarea, max, editor) {
+    //参数：最大限制数，编辑器editor对象
+    KISSY.wordcount.bind = function(max, editor) {
+        var textarea = new S.Node(editor.textarea);
         //在当前text编辑器后面加入操作节点
         var size = 0;
-        S.DOM.insertAfter(S.DOM.create('<div class="J_WS">源码:已输入 <em class="J_WordSize">' + size + '</em>/最多输入 <em class="J_WsMax">' + max + '</em> <span class="J_WsTips"></span></div>'), S.one(textarea).parent('.ke-editor-wrap'));
-        var wordsizenode = S.one(textarea).parent('.ke-editor-wrap').next('.J_WS').children('.J_WordSize');
+        S.DOM.insertAfter(S.DOM.create('<div class="J_WS">源码:已输入 ' +
+            '<em class="J_WordSize">' + size + '</em>/最多输入 ' +
+            '<em class="J_WsMax">' + max + '</em> <span class="J_WsTips"></span></div>'),
+            textarea.parent('.ke-editor-wrap'));
+        var wordsizenode = textarea.parent('.ke-editor-wrap').next('.J_WS')
+            .children('.J_WordSize');
         var tips = "请减少源码数量，否则无法发布成功";
         S.DOM.css('.J_WordSize', {'font-weight':'bold','color':'green'});
         S.DOM.css('.J_WsMax', 'font-weight', 'bold');
@@ -29,7 +37,7 @@
         editor.ready(function() {
             _change(wordsizenode, editor.getData().length);
             editor.on('save restore', function(ev) {
-               
+
                 if (ev.buffer) {
                     timer && clearTimeout(timer);
                     timer = setTimeout(function() {
