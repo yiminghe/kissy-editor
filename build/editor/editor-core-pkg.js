895ac2db0f3a7bfa0a8369dc798cd832453f6ee0
yiminghe <yiminghe@gmail.com>
2012-01-09 11:11:31 +0800

    update demo

--- build/editor/editor-core-pkg.js
+++ build/editor/editor-core-pkg.js
@@ -3,7 +3,7 @@
  *      thanks to CKSource's intelligent work on CKEditor
  * @author yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2
- * @buildtime: 2012-01-09 10:31:54
+ * @buildtime: 2012-01-09 11:11:07
  */
 
 /**
@@ -108,12 +108,12 @@ KISSY.add("editor/export", function(S) {
     if (parseFloat(S.version) < 1.2) {
         getJSName = function () {
             return "plugin-min.js?t=" +
-                encodeURIComponent("2012-01-09 10:31:54");
+                encodeURIComponent("2012-01-09 11:11:07");
         };
     } else {
         getJSName = function (m, tag) {
             return m + '/plugin-min.js' + (tag ? tag : '?t=' +
-                encodeURIComponent('2012-01-09 10:31:54'));
+                encodeURIComponent('2012-01-09 11:11:07'));
         };
     }
 
