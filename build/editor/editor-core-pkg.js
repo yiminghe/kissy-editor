fe13d1bbfd6ca97c5f6d1ad99f9f18c3e86752b7
yiminghe <yiminghe@gmail.com>
2012-02-29 15:26:40 +0800

    update img up dialog

--- build/editor/editor-core-pkg.js
+++ build/editor/editor-core-pkg.js
@@ -3,7 +3,7 @@
  *      thanks to CKSource's intelligent work on CKEditor
  * @author yiminghe@gmail.com, lifesinger@gmail.com
  * @version: 2
- * @buildtime: 2012-02-29 10:31:37
+ * @buildtime: 2012-02-29 15:26:05
  */
 
 /**
@@ -108,12 +108,12 @@ KISSY.add("editor/export", function(S) {
     if (parseFloat(S.version) < 1.2) {
         getJSName = function () {
             return "plugin-min.js?t=" +
-                encodeURIComponent("2012-02-29 10:31:37");
+                encodeURIComponent("2012-02-29 15:26:05");
         };
     } else {
         getJSName = function (m, tag) {
             return m + '/plugin-min.js' + (tag ? tag : '?t=' +
-                encodeURIComponent('2012-02-29 10:31:37'));
+                encodeURIComponent('2012-02-29 15:26:05'));
         };
     }
 
